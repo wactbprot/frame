d24b01e156eef09381e53ce9a732d89416ba380b
wactbprot <thsteinbock@web.de>
2015-06-10 11:20:45 +0200

    index

--- frame.js
+++ frame.js
@@ -1,14 +1,16 @@
 var frame = function(){
   var prog    = require("commander")
     , _       = require("underscore")
-    , bunyan  = require("bunyan")
+
+    , restify = require("restify")
     , coll    = require("./lib/collections")
     , jsnhtml = require("./lib/jsnhtml")
     , deflt   = require("./lib/default")
+
     , mem     = require("ndata").createClient({port: deflt.mem.port})
-    , restify = require("restify")
+
     , server  = restify.createServer({name: deflt.appname})
-    , log     = bunyan.createLogger({name: deflt.appname})
+
 
   prog.version("0.0.1")
   .parse(process.argv);
@@ -22,7 +24,18 @@ var frame = function(){
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     return next();
   });
-  
+
+  server.get( "/css/:file", restify.serveStatic({
+    'directory': __dirname
+  }));
+  server.get( "/fonts/:file", restify.serveStatic({
+    'directory': __dirname
+  }));
+  server.get( "/js/:file", restify.serveStatic({
+    'directory': __dirname
+  }));
+
+
   server.get("/:id/:container/elements", function(req, res, next){
     res.writeHead(200, {
       'Content-Type': 'text/html'
@@ -36,13 +49,32 @@ var frame = function(){
     next();
   });
 
+  server.get("/:id/:container/state", function(req, res, next){
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    coll.get_task_state(req, function(jsn){
+      jsnhtml.task_state(req, jsn, function(html){
+        res.write(html);
+        res.end();
+      });
+    });
+    next();
+  });
+  server.get("/:id/:container/:struct/frame", function(req, res, next){
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    coll.get_mp(req, function(jsn){
+      jsnhtml.frame(req, jsn, function(html){
+        res.write(html);
+        res.end();
+      });
+    });
+    next();
+  });
+
   server.listen(deflt.frame.port, function() {
-    log.info({ok: true}
-            , "``````````````````````````````\n"
-            + "frame server up and running @"
-            + deflt.frame.port +"\n"
-            + "``````````````````````````````"
-            );
   });
 
 }
