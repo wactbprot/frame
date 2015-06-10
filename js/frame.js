0ead9b52c4a4c72a9bcb05c9432d27f70d75c397
wactbprot <thsteinbock@web.de>
2015-06-10 15:37:40 +0200

    poll state ok, +rdm

--- js/frame.js
+++ js/frame.js
@@ -1,11 +1,24 @@
 (function($){
 
- var $content_link =  $("#content_link")
+  // once at the begining
+  var $content_link =  $("#content_link")
   var url = $content_link.attr("href")
-  $.get( url, function( data ) {
+  $.get( url, function( html ) {
     $content_link.replaceWith( function() {
-      return $(data).hide().fadeIn();
+      return $(html).hide().fadeIn(300);
     });
+  });
 
-});
+  setInterval(function(){
+    // poll state
+    var $state =  $(".state");
+    if($state.length > 0){
+      var path = $state.data("path")
+      console.log()
+      $.get( path, function( html ) {
+        $state.replaceWith( html );
+      });
+    }
+  }, 500)
+
 })(jQuery);
