7a557c6419df3c364b724451c947bb8b4d087314
wactbprot <thsteinbock@web.de>
2015-06-11 13:23:37 +0200

    poll batch dont work

--- js/frame.js
+++ js/frame.js
@@ -9,15 +9,39 @@
     });
   });
 
+  var O = 3
+    , N = N || O
+    , M = M || 0
+
   setInterval(function(){
     // poll state
     var $state =  $(".state");
     if($state.length > 0){
       var path = $state.data("path")
-      console.log()
       $.get( path, function( html ) {
         $state.replaceWith( html );
       });
+    }
+    // poll elements
+    var $poll =  $(".poll")
+      , L = $poll.length
+    if( L > 0){
+      for(var i = 0; i < L; i++){
+        if(M > L){
+          M = 0;
+          N = O;
+        }
+        if(i < N && i >= M){
+          var path = $poll.eq(i).data("path");
+          $.get( path, function( html ) {
+            $poll.eq(i).parent().replaceWith( html );
+          });
+        }else{
+          M = M + O
+          N = N + O
+          break;
+        }
+      };
     }
   }, 500)
 
